536605abeb4ba05cf66e4ce031cc744566b2f59c
Milko Venkov <MVenkov@infragistics.com>
2021-12-16 17:42:25 +0200

    initial commit

---
+++ SerializeNumberToStringIssue/Data/Customer.cs
@@ -0,0 +1,16 @@
+﻿namespace SerializeNumberToStringIssue.Data
+{
+    public class Customer
+    {
+        public string? CustomerID { get; set; }
+
+        public string? CompanyName { get; set; }
+
+        public string? Address { get; set; }
+
+        public string? City { get; set; }
+
+        public string? PostalCode { get; set; }
+
+    }
+}
